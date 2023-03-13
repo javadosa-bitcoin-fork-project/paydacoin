7d4459b3fa61c128fefa4c5ee7242fd6ae3a000e
Ryan H. Lee <javadosa@gmail.com>
2023-03-13 11:17:39 +0900

    change the initial block hash value in genesis block generation

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -112,7 +112,7 @@ public:
 
         genesis = CreateGenesisBlock(1678341585, 720155, 0x1e0ffff0, 1, 10 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
+        assert(consensus.hashGenesisBlock == uint256S("0x00000f4d819b2405fe05d9cb6b730d31be7d58dd749b825b3fdcc93e0d5f2cd0"));
         assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
 
         // Note that of those which support the service bits prefix, most only support a subset of
@@ -227,7 +227,7 @@ public:
 
         genesis = CreateGenesisBlock(1678341585, 720155, 0x1e0ffff0, 1, 10 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"));
+        assert(consensus.hashGenesisBlock == uint256S("0x00000f4d819b2405fe05d9cb6b730d31be7d58dd749b825b3fdcc93e0d5f2cd0"));
         assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
 
         vFixedSeeds.clear();
@@ -434,7 +434,7 @@ public:
 
         genesis = CreateGenesisBlock(1678341585, 720155, 0x1e0ffff0, 1, 10 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206"));
+        assert(consensus.hashGenesisBlock == uint256S("0x00000f4d819b2405fe05d9cb6b730d31be7d58dd749b825b3fdcc93e0d5f2cd0"));
         assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
 
         vFixedSeeds.clear(); //!< Regtest mode doesn't have any fixed seeds.
